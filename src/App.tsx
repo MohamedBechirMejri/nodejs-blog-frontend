3a7d57334f367a65a91a0b31a36119684f0ddfa3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-24 13:48:45 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Home</h1>
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
