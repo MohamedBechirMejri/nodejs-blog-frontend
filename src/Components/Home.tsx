3af68f1575432daf9a984984e64d9e2010415b94
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-25 06:56:42 +0100

    get data from server

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
 import React from "react";
+import ArticleType from "../Types/Article";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [articles, setArticles] = React.useState([] as ArticleType[]);
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:3000/articles")
+      .then(response => {
+        console.log(response.data);
+        setArticles(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {articles.map(article => (
+        <div>{article.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
