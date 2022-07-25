d3b6f225405dc10d965f6180308133b84c3df4b4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-25 19:15:52 +0100

    tweak articles

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -19,12 +19,12 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-start p-8 gap-20 py-32">
       {articles.map(article => {
-        const { title, author, image, likes, createdAt, _id } = article;
+        const { title, author, image, likes, _id } = article;
         return (
           <Link
             to={`/articles/${_id}`}
             key={_id}
-            className=" h-[60vh] rounded-xl w-[90vw] flex flex-col justify-end items-start text-white font-bold text-xl overflow-hidden transition-all hover:ring-4 ring-gray-500 "
+            className=" h-[30vh] rounded-[2.5rem] w-[85vw] flex flex-col justify-end items-start text-white font-bold overflow-hidden transition-all hover:ring-4 ring-gray-500 "
             style={{
               backgroundImage: `url(${image})`,
               backgroundSize: "cover",
@@ -32,12 +32,15 @@ const Home = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className="p-4 bg-[#00000055] w-full ">
-              <h1 className="text-white text-3xl">{title}</h1>
-              <p className="text-white text-xl">{`${author.firstName} ${author.lastName}`}</p>
-              <p className="text-white text-xl">{likes.length} Likes</p>
-              <p className="text-white text-xl">
-                {new Date(createdAt).toLocaleString()}
+            <div className="p-4 bg-[#00000055] w-full h-full relative">
+              <h1 className="text-white text-3xl p-4 ">{title}</h1>
+              <p className="text-white text-xl absolute bottom-8 left-12 p-2 ">{`${author.firstName} ${author.lastName}`}</p>
+
+              <p
+                className="text-white absolute bottom-8 right-12 bg-[#40507855] p-2 rounded-full font-medium px-4 backdrop-blur-3xl"
+                style={{}}
+              >
+                {likes.length} Likes
               </p>
             </div>
           </Link>
