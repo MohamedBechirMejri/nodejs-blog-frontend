c432d952315e322fdce8af71d759bb7d0ae2bb14
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-25 06:16:16 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Article from "./Components/Article";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Home</h1>
-              </div>
-            }
-          ></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/articles/:id" element={<Article />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
