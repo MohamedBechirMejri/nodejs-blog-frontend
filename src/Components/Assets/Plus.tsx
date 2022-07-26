98b3a0f60d38b88754b86cd41cbbf3ee276d7872
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-26 05:59:59 +0100

    tweak plus icon

--- src/Components/Assets/Plus.tsx
+++ src/Components/Assets/Plus.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Plus = () => {
   return (
     <svg
-      width="24"
-      height="24"
+      width="46"
+      height="46"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,7 @@ const Plus = () => {
         stroke-linecap="round"
         stroke-linejoin="round"
       />
-      <path
+      {/* <path
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M16.6857 2H7.31429C4.04762 2 2 4.31208 2 7.58516V16.4148C2 19.6879 4.0381 22 7.31429 22H16.6857C19.9619 22 22 19.6879 22 16.4148V7.58516C22 4.31208 19.9619 2 16.6857 2Z"
@@ -31,7 +31,7 @@ const Plus = () => {
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
-      />
+      /> */}
     </svg>
   );
 };
