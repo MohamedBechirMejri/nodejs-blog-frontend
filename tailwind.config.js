f72f32b1bd8261fc8a4a6f79ec0eed088c413fd1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-26 09:10:42 +0100

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        reveal: "reveal 0.3s ease-in-out forwards",
+        hide: "hide 0.3s ease-in-out forwards",
+      },
+      keyframes: {
+        reveal: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-50%, 300%)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%, -50%)",
+          },
+        },
+        hide: {
+          "0%": {
+            opacity: 1,
+            transform: "translate(-50%, -50%)",
+          },
+          "100%": {
+            opacity: 0,
+            transform: "translate(-50%, 300%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
