6fe5cd72a5e99260d1bad96e045957a1c5b54c4c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-26 06:23:06 +0100

    add some tweaks

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,29 +8,59 @@ import Profile from "./Assets/Profile";
 import Search from "./Assets/Search";
 
 const Navbar = () => {
+  const [currentTab, setCurrentTab] = React.useState("home");
+
   return (
-    <nav className="fixed z-50 bottom-2 left-1/2 w-[95%] -translate-x-1/2 h-16 flex items-center justify-center gap-8 p-4 rounded-full bg-[#FAF9FE] text-[#B1B5C8] stroke-[#B1B5C8]">
-      <div className="flex items-center justify-evenly p-4 gap-4">
-        <Link to="/">
+    <nav className="fixed z-50 bottom-2 left-1/2 w-[95%] -translate-x-1/2 h-16 flex items-center justify-center gap-4 p-4 rounded-full bg-[#FAF9FE] text-[#B1B5C8] stroke-[#aaaaaa]">
+      <div className="flex items-center justify-evenly p-4 gap-8">
+        <Link
+          to="/"
+          style={{
+            stroke: currentTab === "home" ? "#6434D1" : "#aaaaaa",
+          }}
+          onClick={() => setCurrentTab("home")}
+          className="transition-all active:scale-95"
+        >
           <Home />
         </Link>
-        <Link to="/articles/1">
+        <Link
+          to="/"
+          style={{
+            stroke: currentTab === "bookmarks" ? "#6434D1" : "#aaaaaa",
+          }}
+          onClick={() => setCurrentTab("bookmarks")}
+          className="transition-all active:scale-95"
+        >
           <Bookmarks />
         </Link>
       </div>
       <Link
-        to="/articles/2"
+        to="/"
         className="bg-red-500 p-8 rounded-full relative -translate-y-6 bg-gradient-to-br from-[#FF9271] to-[#FF7695] text-white font-bold hover:ring ring-[#FFEBED] transition-all active:scale-90"
       >
         <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 stroke-white">
           <Plus />
         </p>
       </Link>
-      <div className="flex items-center justify-evenly p-4 gap-4">
-        <Link to="/articles/2">
+      <div className="flex items-center justify-evenly p-4 gap-8">
+        <Link
+          to="/"
+          style={{
+            stroke: currentTab === "search" ? "#6434D1" : "#aaaaaa",
+          }}
+          onClick={() => setCurrentTab("search")}
+          className="transition-all active:scale-95"
+        >
           <Search />
         </Link>
-        <Link to="/articles/3">
+        <Link
+          to="/"
+          style={{
+            stroke: currentTab === "profile" ? "#6434D1" : "#aaaaaa",
+          }}
+          onClick={() => setCurrentTab("profile")}
+          className="transition-all active:scale-95"
+        >
           <Profile />
         </Link>
       </div>
