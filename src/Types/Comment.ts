6cc458d64f1a938c4aa205e0f92b4c4d58b5d8c8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 18:29:12 +0100

    add user type

--- src/Types/Comment.ts
+++ src/Types/Comment.ts
@@ -1,6 +1,11 @@
 type CommentType = {
   body: string;
-  user: string;
+  user: {
+    firstName: string;
+    lastName: string;
+    picture: string;
+    _id: string;
+  };
   __v: number;
   _id: string;
   createdAt: string;
