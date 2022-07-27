9207a5936fb4d31b753aa2d474315fdf366e4e10
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 08:57:18 +0100

    center loader

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   const style = "p-[.4rem] bg-[#F26865] rounded-full animate-bounce2";
   return (
-    <div className="flex items-center justify-center gap-1 ">
+    <div className="absolute flex items-center justify-center gap-1 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <div className={style + " [animation-delay:.2s]"}></div>
       <div className={style + " [animation-delay:.1s]"}></div>
       <div className={style + "  "}></div>
