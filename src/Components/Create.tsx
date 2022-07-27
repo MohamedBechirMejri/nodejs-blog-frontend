b6fbe1d84bc0529f8c77100a0b3ead9ccec1db5b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 06:16:18 +0100

    style create component

---
+++ src/Components/Create.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Create = () => {
+  const [categories, setCategories] = React.useState(
+    [] as {
+      _id: number;
+      name: string;
+    }[]
+  );
+  useEffect(() => {
+    const cats = [
+      {
+        _id: 1,
+        name: "Technology",
+      },
+      {
+        _id: 2,
+        name: "Business",
+      },
+      {
+        _id: 3,
+        name: "Entertainment",
+      },
+      {
+        _id: 4,
+        name: "General",
+      },
+    ];
+    setCategories(cats);
+  }, []);
+
+  return (
+    <form className="flex flex-col items-center justify-center w-full h-full min-h-screen gap-8 p-8 pb-32 animate-revealPage">
+      <h1 className="text-3xl font-medium text-center">
+        Add Article <br />
+      </h1>
+      <input
+        type="text"
+        className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
+        placeholder="Title"
+        required
+        minLength={5}
+      />
+      <textarea
+        className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
+        placeholder="Body"
+        required
+        minLength={250}
+      />
+      <input
+        type="text"
+        className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
+        placeholder="Image (https://picsum/photos/700)"
+        required
+      />
+      <select
+        className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
+        placeholder="Category"
+        required
+      >
+        {categories.map(category => (
+          <option
+            key={category._id}
+            value={category._id}
+            className="text-xl font-medium transition-all "
+          >
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <button className="w-full p-5 rounded-lg bg-[#F26865] text-white text-xl font-medium shadow-sm shadow-[#F26865] active:bg-[#ca4747] transition-all">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Create;
