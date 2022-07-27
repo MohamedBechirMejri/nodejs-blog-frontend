e74180e7a72412713b2336f0b8bbe80d05ee69c3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 08:57:48 +0100

    add loader

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ArticleType from "../Types/Article";
 import Heart from "./Assets/Heart";
+import Loader from "./Loader";
 
 const Search = () => {
   const [articles, setArticles] = React.useState([] as ArticleType[]);
@@ -52,32 +53,36 @@ const Search = () => {
         required
       />
       <div className="flex flex-col items-center justify-start gap-20 p-8 pt-8 pb-32 ">
-        {articles.map((article, i) => {
-          const { title, author, image, likes, _id } = article;
-          return (
-            <Link
-              to={`/articles/${_id}`}
-              key={_id}
-              className=" h-[30vh] rounded-[2.5rem] w-[85vw] flex flex-col justify-end items-start text-white font-bold overflow-hidden transition-all hover:ring-4 ring-gray-500 animate-revealPage opacity-0"
-              style={{
-                backgroundImage: `url(${image})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                animationDelay: `${(i + 1) * 0.1}s`,
-              }}
-            >
-              <div className="p-4 bg-[#00000055] w-full h-full relative">
-                <h1 className="p-4 text-3xl text-white ">{title}</h1>
-                <p className="absolute p-2 text-xl text-white bottom-8 left-12 ">{`${author.firstName} ${author.lastName}`}</p>
+        {articles.length === 0 ? (
+          <Loader />
+        ) : (
+          articles.map((article, i) => {
+            const { title, author, image, likes, _id } = article;
+            return (
+              <Link
+                to={`/articles/${_id}`}
+                key={_id}
+                className=" h-[30vh] rounded-[2.5rem] w-[85vw] flex flex-col justify-end items-start text-white font-bold overflow-hidden transition-all hover:ring-4 ring-gray-500 animate-revealPage opacity-0"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  animationDelay: `${(i + 1) * 0.1}s`,
+                }}
+              >
+                <div className="p-4 bg-[#00000055] w-full h-full relative">
+                  <h1 className="p-4 text-3xl text-white ">{title}</h1>
+                  <p className="absolute p-2 text-xl text-white bottom-8 left-12 ">{`${author.firstName} ${author.lastName}`}</p>
 
-                <p className="text-white absolute bottom-8 right-12 bg-[#40507855] p-2 rounded-full font-medium px-4 backdrop-blur-3xl stroke-current flex items-center justify-center gap-4">
-                  <Heart /> {likes.length}
-                </p>
-              </div>
-            </Link>
-          );
-        })}
+                  <p className="text-white absolute bottom-8 right-12 bg-[#40507855] p-2 rounded-full font-medium px-4 backdrop-blur-3xl stroke-current flex items-center justify-center gap-4">
+                    <Heart /> {likes.length}
+                  </p>
+                </div>
+              </Link>
+            );
+          })
+        )}
       </div>
     </div>
   );
