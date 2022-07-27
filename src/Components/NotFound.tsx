516c0b8614c12fbee19c1a7023d35f4a94f90b40
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 07:29:52 +0100

    style not found page

--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-4 text-3xl font-bold animate-revealPage">
+      <h1 className=" text-[#F26865] ">404</h1>
+      <h2 className="">Not Found</h2>
+    </div>
+  );
 };
 
 export default NotFound;
