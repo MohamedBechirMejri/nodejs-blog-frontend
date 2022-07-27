22c08b3e9a8400ebc35865db3c25fb14c66365b5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 12:49:27 +0100

    setup create functions

--- src/Components/Create.tsx
+++ src/Components/Create.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Create = () => {
   const [categories, setCategories] = React.useState(
@@ -10,6 +11,38 @@ const Create = () => {
     }[]
   );
   const navigate = useNavigate();
+  const [token, setToken] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [body, setBody] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [image, setImage] = React.useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:3000/articles",
+        {
+          title,
+          body,
+          category,
+          image,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then(res => {
+        navigate(`/articles/${res.data._id}`);
+      })
+      .catch(err => {
+        err.response.data.errors.forEach((err: { msg: string }) => {
+          toast.error(err.msg, {});
+        });
+      });
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -17,6 +50,7 @@ const Create = () => {
     if (!token) {
       navigate("/login");
     } else {
+      setToken(token);
       axios
         .get("http://localhost:3000/categories")
         .then(response => {
@@ -29,7 +63,10 @@ const Create = () => {
   }, [navigate]);
 
   return (
-    <form className="flex flex-col items-center justify-center w-full h-full min-h-screen gap-8 p-8 pb-32 animate-revealPage">
+    <form
+      className="flex flex-col items-center justify-center w-full h-full min-h-screen gap-8 p-8 pb-32 animate-revealPage"
+      onSubmit={handleSubmit}
+    >
       <h1 className="text-3xl font-medium text-center">
         Add Article <br />
       </h1>
@@ -39,23 +76,31 @@ const Create = () => {
         placeholder="Title"
         required
         minLength={5}
+        value={title}
+        onChange={e => setTitle(e.target.value)}
       />
       <textarea
         className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
         placeholder="Body"
         required
         minLength={250}
+        value={body}
+        onChange={e => setBody(e.target.value)}
       />
       <input
         type="text"
         className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
         placeholder="Image (https://picsum/photos/700)"
         required
+        value={image}
+        onChange={e => setImage(e.target.value)}
       />
       <select
         className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
         placeholder="Category"
         required
+        value={category}
+        onChange={e => setCategory(e.target.value)}
       >
         {categories.map(category => (
           <option
