973fb95a5d9ac75280bfe1e488704d5d18aa2cc8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 19:04:06 +0100

    add comment submition

--- src/Components/Comments.tsx
+++ src/Components/Comments.tsx
@@ -1,13 +1,58 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ArticleType from "../Types/Article";
 import CommentType from "../Types/Comment";
 
 const Comments = ({
   className,
   Comments,
+  setArticle,
 }: {
   className?: string;
   Comments: CommentType[];
+  setArticle: (article: ArticleType) => void;
 }) => {
+  const { id } = useParams();
+  const [comment, setComment] = React.useState("");
+  const [token, setToken] = React.useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (token) {
+      axios
+        .post(
+          `http://localhost:3000/articles/${id}/comment`,
+          {
+            body: comment,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then(res => {
+          setArticle(res.data);
+          setComment("");
+        })
+        .catch(err => {
+          console.log(err.response.data);
+        });
+    } else {
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
   return (
     <div className={`${className} w-full px-8`}>
       <h1 className="text-3xl font-bold">Comments:</h1>
@@ -33,13 +78,15 @@ const Comments = ({
       <form
         className="flex flex-col items-center justify-center w-full h-full gap-8 py-20 animate-revealPage"
         autoComplete="on"
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
       >
         <h1 className="text-3xl font-medium text-center">Add Comment!</h1>
         <textarea
           className="w-full p-5 transition-all rounded-lg border-white focus:border-[#F26865] focus:ring-[#F26865] outline-none placeholder:font-medium"
           placeholder="Enter Text"
           required
+          value={comment}
+          onChange={e => setComment(e.target.value)}
         />
         <button className="w-full p-5 rounded-lg bg-[#F26865] text-white text-xl font-medium shadow-sm shadow-[#F26865] active:bg-[#ca4747] transition-all">
           Submit
