0556280b3ec7d8df277389d886847b1c9f9c127b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-27 22:15:44 +0100

    add nav home button to 404

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen gap-4 text-3xl font-bold animate-revealPage">
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-6 p-12 text-3xl font-bold animate-revealPage">
       <h1 className=" text-[#F26865] ">404</h1>
       <h2 className="">Not Found</h2>
+      <button
+        className="w-full p-4 rounded-lg bg-[#f26765e8] text-white text-xl font-medium shadow-sm shadow-[#F26865] active:scale-90 transition-all"
+        onClick={() => navigate("/")}
+      >
+        Go Home!
+      </button>
     </div>
   );
 };
